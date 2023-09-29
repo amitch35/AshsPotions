d55ef31b6e745bfb43e5db6d2abbbae5e53ffd7c
Ash Mitchell <amitch35@calpoly.edu>
2023-09-29 12:00:22 -0700

    Barrels V1

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -32,8 +32,21 @@ def list_viable(gold: int, catalog: list[Barrel]):
 def post_deliver_barrels(barrels_delivered: list[Barrel]):
     """ """
     print(barrels_delivered)
-
-    return "OK"
+    if barrels_delivered:
+        red_ml_received = 0
+        # green_ml_received = 0
+        # blue_ml_received = 0
+        # dark_ml_received = 0
+        gold_spent = 0
+        for type in barrels_delivered:
+            red_ml_received = type.ml_per_barrel * type.quantity
+            gold_spent += type.price * type.quantity
+        with db.engine.begin() as connection:
+            sql = f"UPDATE global_inventory SET gold = gold - {gold_spent}, num_red_ml = num_red_ml + {red_ml_received}"
+            connection.execute(sqlalchemy.text(sql))
+        return "OK"
+    else:
+        return "Nothing Delivered"
 
 # Gets called once a day
 @router.post("/plan")
@@ -43,17 +56,17 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
-        for row in result:
-            print(row)
-            if row[3] < 10:
-                options = list_viable(row[1], wholesale_catalog) # check afford and quantity in catalog
-                if len(options) > 0:
-                        return [
-                            {
-                                "sku": "SMALL_RED_BARREL",
-                                "quantity": 1,
-                            }
-                        ]
+        inv = result.first() # inventory is on a single row
+        print(inv)
+        if inv.num_red_potions < 10:
+            options = list_viable(inv.gold, wholesale_catalog) # check afford and quantity in catalog
+            if len(options) > 0:
+                    return [
+                        {
+                            "sku": "SMALL_RED_BARREL",
+                            "quantity": 1,
+                        }
+                    ]
         return
 
 
