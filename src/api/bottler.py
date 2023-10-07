e31288cfc6e06607ba4f8b2ac9fdd1e2d9437b88
Ash Mitchell <amitch35@calpoly.edu>
2023-10-06 22:19:26 -0700

    bottler.py

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy
 from src import database as db
-from api import audit
+from src.api import audit
 
 class Color(Enum):
     RED = 0
