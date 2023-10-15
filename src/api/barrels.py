c3a0df8c433e06379ee51251dec7f117f070a4a4
Ash Mitchell <amitch35@calpoly.edu>
2023-10-15 01:08:42 -0700

    barrels.py

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -174,6 +174,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                         if i == NUM_COLORS: # Check if need to cycle through again
                             i = 0
                         if barrel is None: # if there are no options for that color
+                            priority = [color for color in priority if color != priority[i]]
                             continue
                         gold -= barrel.price
                         # Check if there is a Barrel with the same SKU already in barrel_plan
