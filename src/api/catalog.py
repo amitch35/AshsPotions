b0f13926dbbd7eba6a0fb3f3357afadbc26874a2
Ash Mitchell <amitch35@calpoly.edu>
2023-10-29 02:47:26 -0700

    catalog.py: Constants and Time based offerings

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
+from enum import IntEnum
+from pydantic import BaseModel
 import sqlalchemy
+from sqlalchemy import *
 from src import database as db
 from src.api.bottler import BOTTLE_THRESHOLD
 
@@ -9,7 +12,67 @@ PHASE_ONE = 1 # Getting started, growth and aquiring customers
 PHASE_TWO = 2 # Optimizing Purchases and offerings
 SHOP_PHASE = PHASE_TWO
 
+BEST_SELLERS = ["red_potion", "green_potion"]
+
 CATALOG_MAX = 6
+
+# Use reflection to derive table schema. You can also code this in manually.
+metadata_obj = sqlalchemy.MetaData()
+potions = sqlalchemy.Table("potions", metadata_obj, autoload_with=db.engine)
+potion_quantities = sqlalchemy.Table("potion_quantities", metadata_obj, autoload_with=db.engine)
+
+class DayOfWeek(IntEnum):
+    SUNDAY = 0
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+
+class Potion(BaseModel):
+    sku: str
+    price: int
+    name: str
+    red: int
+    green: int
+    blue: int
+    dark: int
+    quantity: int
+
+def add_best_sellers(catalog: list[Potion], potions):
+    num_added = 0
+    for potion in potions:
+        if potion.sku in BEST_SELLERS and potion.sku not in [potion.sku for potion in catalog]:
+            catalog.append(potion)
+            num_added += 1
+    return num_added
+
+def list_exclusions(day_of_week):
+    # Some potions don't sell well on certain days of the week
+    match day_of_week:
+        case DayOfWeek.SUNDAY:
+            print("Today is Sunday.")
+            exclude = []
+        case DayOfWeek.MONDAY:
+            print("Today is Monday.")
+            exclude = ["blue_potion"]
+        case DayOfWeek.TUESDAY:
+            print("Today is Tuesday.")
+            exclude = []
+        case DayOfWeek.WEDNESDAY:
+            print("Today is Wednesday.")
+            exclude = []
+        case DayOfWeek.THURSDAY:
+            print("Today is Thursday.")
+            exclude = []
+        case DayOfWeek.FRIDAY:
+            print("Today is Friday.")
+            exclude = ["purple_potion"]
+        case DayOfWeek.SATURDAY:
+            print("Today is Saturday.")
+            exclude = ["purple_potion"]
+    return exclude
 
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
@@ -19,26 +82,70 @@ def get_catalog():
 
     # Can return a max of 6 items.
     print("----Catalog----")
-    with db.engine.begin() as connection:
-        # TODO: Implement best sellers/constants and time based offerings 
-        sql = ("SELECT potions.*, "
-                        "COALESCE(SUM(potion_quantities.delta), 0) AS quantity "
-                    "FROM potions "
-                    "JOIN potion_quantities ON "
-                        "potions.id = potion_quantities.potion_id "
-                    "GROUP BY potions.id "
-                    "HAVING coalesce(sum(potion_quantities.delta), 0) > 0 "
-                    "ORDER BY RANDOM() "
-                    f"LIMIT {CATALOG_MAX}")
-        result = connection.execute(sqlalchemy.text(sql))
+    with db.engine.begin() as conn:
+        # Implements best sellers and time based offerings before randomly offering others
         catalog = []
-        for potion in result:
+        catalog_size = 0
+        day_of_week = conn.execute(select([func.extract('dow', func.now())]))
+        stmt = (
+            select(
+                [potions, func.coalesce(func.sum(potion_quantities.c.delta), 0).label("quantity")]
+            )
+            .select_from(
+                potions.join(potion_quantities, potions.c.id == potion_quantities.c.potion_id)
+            )
+            .group_by(
+                potions.c.id
+            )
+            .having(
+                func.coalesce(func.sum(potion_quantities.c.delta), 0) > 0
+            )
+        )
+        # exclude some potions on certain days
+        exclusions = list_exclusions(day_of_week)
+        if len(exclusions) > 0:
+            stmt = (
+                stmt.where(
+                    and_(
+                        not_(potions.c.sku.in_(exclusions))
+                    )
+                )
+            )
+        all_available_potions = conn.execute(sqlalchemy.text(stmt))
+        catalog_size += add_best_sellers(catalog, all_available_potions)
+        # make sure that no duplicates can be returned by susequent queries
+        stmt = (
+            stmt.where(
+                and_(
+                    not_(potions.c.sku.in_([potion.sku for potion in catalog]))
+                )
+            )
+        )
+        # remaining catalog is generated randomly
+        num_needed = CATALOG_MAX - catalog_size
+        if num_needed > 0:
+            stmt = (
+                stmt.order_by(
+                    text("RANDOM()")
+                )
+                .limit(num_needed)
+            )
+            result = conn.execute(stmt)
+            for potion in result:
+                catalog.append(potion)
+        
+        catalog_json = []
+        for potion in catalog:
             print(f"{potion.name}: {potion.quantity}")
-            catalog.append({
+            if SHOP_PHASE == PHASE_ONE:
+                qty = potion.quantity
+            elif SHOP_PHASE == PHASE_TWO:
+                qty = BOTTLE_THRESHOLD
+            catalog_json.append({
                         "sku": potion.sku,
                         "name": potion.name,
-                        "quantity": BOTTLE_THRESHOLD,
+                        "quantity": qty,
                         "price": potion.price,
                         "potion_type": [potion.red, potion.green, potion.blue, potion.dark],
                     })
-        return catalog
+        return catalog_json
