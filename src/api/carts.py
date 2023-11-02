3abbec1abc58740a2bac03f96d2c78e37326517d
Ash Mitchell <amitch35@calpoly.edu>
2023-11-02 10:20:10 -0700

    carts.py: Checkout simplification

--- src/api/carts.py
+++ src/api/carts.py
@@ -282,63 +282,45 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
     print(f"----Cart {cart_id} Checkout----")
     try:
-        #.execution_options(isolation_level="SERIALIZABLE")
         with db.engine.begin() as connection:
-            sql = f"SELECT * FROM transactions WHERE cart_id = {cart_id} ORDER BY success desc; "
+            #print(f"Cart {cart_id}: {cart}")
+            sql = (f"SELECT *, "
+                    "(SELECT sum(delta) FROM potion_quantities "
+                    "WHERE potion_id = cnt.potion_id) AS quantity "
+                    "FROM cart_contents AS cnt "
+                    f"JOIN potions AS pot ON cnt.potion_id = pot.id "
+                    f"WHERE cart_id = {cart_id}; ")
             result = connection.execute(sqlalchemy.text(sql))
-            prev_transaction = result.first()
-            if (prev_transaction is None) or (prev_transaction.success == False):
-                sql = f"SELECT * FROM shopping_carts WHERE id = {cart_id}; "
-                result = connection.execute(sqlalchemy.text(sql))
-                cart = result.first()
-                #print(f"Cart {cart_id}: {cart}")
-                if cart: # if there exists a cart with the given id
-                    sql = (f"SELECT *, "
-                           "(SELECT sum(delta) FROM potion_quantities "
-                           "WHERE potion_id = cnt.potion_id) AS quantity "
-                           "FROM cart_contents AS cnt "
-                            f"JOIN potions AS pot ON cnt.potion_id = pot.id "
-                            f"WHERE cart_id = {cart_id}; ")
-                    result = connection.execute(sqlalchemy.text(sql))
-                    transaction = False
-                    total = 0
-                    selling = 0
-                    # check validity of cart and determine total price
-                    for record in result:
-                        if record.quantity < record.quantity_requested:
-                            print(f"Cart with id {cart_id} requested too many potions (insufficient stock)")
-                            raise HTTPException(
-                                status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden: requested too many potions"
-                            )
-                        selling += record.quantity_requested
-                        total += record.price * record.quantity_requested
-                    if selling == 0:
-                        raise HTTPException(
-                            status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request: Cart was empty"
-                        )
-                    # execute transaction 
-                    #print(f"Cart {cart_id} Completing transaction for {cart.customer}")
-                    sql = ("INSERT INTO global_inventory "
-                        "(gold, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml)"
-                        f" VALUES ({total}, 0, 0, 0, 0); \n")
-                    sql += ("INSERT INTO potion_quantities (potion_id, delta) "
-                        "SELECT potion_id, - quantity_requested "
-                        "FROM cart_contents "
-                        f"WHERE cart_id = {cart_id}; ")
-                    connection.execute(sqlalchemy.text(sql))
-                    transaction = True
-                    sql = (f"INSERT INTO transactions (cart_id, success, payment, gold_paid) "
-                        f"VALUES ({cart_id}, {transaction}, '{cart_checkout.payment}', {total}); ")
-                    connection.execute(sqlalchemy.text(sql))
+            transaction = False
+            total = 0
+            selling = 0
+            # check validity of cart and determine total price
+            for record in result:
+                if record.quantity < record.quantity_requested:
+                    print(f"Cart with id {cart_id} requested too many potions (insufficient stock)")
+                    raise HTTPException(
+                        status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden: requested too many potions"
+                    )
+                selling += record.quantity_requested
+                total += record.price * record.quantity_requested
+            if selling == 0:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request: Cart did not exist or was empty"
+                )
+            # execute transaction 
+            #print(f"Cart {cart_id} Completing transaction for {cart.customer}")
+            transaction = True
+            sql = ("INSERT INTO global_inventory "
+                "(gold, num_red_ml, num_green_ml, num_blue_ml, num_dark_ml)"
+                f" VALUES ({total}, 0, 0, 0, 0); \n")
+            sql += ("INSERT INTO potion_quantities (potion_id, delta) "
+                "SELECT potion_id, - quantity_requested "
+                "FROM cart_contents "
+                f"WHERE cart_id = {cart_id}; ")
+            sql += (f"INSERT INTO transactions (cart_id, success, payment, gold_paid) "
+                f"VALUES ({cart_id}, {transaction}, '{cart_checkout.payment}', {total}); ")
+            connection.execute(sqlalchemy.text(sql))
 
-                    return {"success": transaction, "total_potions_bought": selling, "total_gold_paid": total}
-                else:
-                    print(f"Cart with id {cart_id} does not exist")
-                    return {"success": False, "total_potions_bought": 0, "total_gold_paid": 0}
-            else:
-                print(f"Cart with id {cart_id} already completed transaction")
-                sql = f"SELECT SUM(quantity_requested) FROM cart_contents WHERE cart_id = {cart_id}"
-                potions_bought = connection.execute(sqlalchemy.text(sql)).scalar_one()
-                return {"success": prev_transaction.success, "total_potions_bought": potions_bought, "total_gold_paid": prev_transaction.gold_paid}
+            return {"success": transaction, "total_potions_bought": selling, "total_gold_paid": total}
     except DBAPIError as error:
         print(f"Error returned: <<<{error}>>>")
